0ac674c5a0e8172590eb0da58326f965a4ad35ec
Graham Vickers <43250387+grahamvickers@users.noreply.github.com>
2022-08-18 23:16:29 -0400

    app is finished, can browse add items to basket and edit basket and place order and track progress

--- App.js
+++ App.js
@@ -6,6 +6,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import RestaurantScreen from './screens/RestaurantScreen';
 import BasketScreen from './screens/BasketScreen';
+import PreparingOrderScreen from './screens/PreparingOrderScreen';
+import DeliveryScreen from './screens/DeliveryScreen';
 import { store } from './store'
 import { Provider } from 'react-redux'
 
@@ -24,6 +26,16 @@ export default function App() {
               name="Basket" 
               component={BasketScreen}
               options={{ presentation: "modal", headerShown: false}}
+            />
+            <Stack.Screen 
+              name="PreparingOrderScreen" 
+              component={PreparingOrderScreen} 
+              options={{ presentation: 'fullScreenModal', headerShown: false }}
+            />
+            <Stack.Screen 
+              name="Delivery" 
+              component={DeliveryScreen} 
+              options={{ presentation: 'fullScreenModal', headerShown: false }}
             />
           </Stack.Navigator>
         </TailwindProvider>
